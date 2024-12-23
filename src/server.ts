25226134655376ea2f1be36c73a83f9395eaeec5
Merfrand LOKONON <gratiaslokonon@gmail.com>
2024-12-23 15:47:06 +0100

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import { routes } from './routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.listen(3000, () => console.log("Server is running on port 3000"));  
